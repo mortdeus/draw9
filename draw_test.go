2b49b761b2c801181e75aa3ec9a03c33bb1efae7
mischief@offblast.org <mischief@offblast.org>
2014-01-07 02:14:06 -0800

    poor man's paint

--- draw_test.go
+++ draw_test.go
@@ -3,7 +3,6 @@ package draw9
 import (
 	"image"
 	"testing"
-	"time"
 )
 
 // try me!
@@ -25,17 +24,41 @@ func TestInitDraw(t *testing.T) {
 	ms := InitMouse("", disp.ScreenImage)
 	//defer ms.Close()
 
+	//var pix []image.Point
+
 loop:
 	for {
 		select {
 		case r := <-kbd.C:
 			// keyboard char
 			t.Logf("kbd: %c", r)
+			if r == 'q' || r == Kdel {
+				break loop
+			}
 		case m := <-ms.C:
 			// mouse move
 			t.Logf("ms: %s", m)
-			pt := m.Pt()
-			img.Draw(image.Rect(pt.X-5, pt.Y-5, pt.X+5, pt.Y+5), disp.Black, nil, image.ZP)
+			if m.Mb1() {
+				var newpt image.Point
+				pt := m.Pt()
+				for m2 := range ms.C {
+					if !m2.Mb1() {
+						break
+					}
+					newpt = m2.Pt()
+					if newpt == pt {
+						continue
+					}
+					img.Line(pt, newpt, 0, 0, 3, disp.Black, image.ZP)
+					disp.Flush()
+					pt = newpt
+				}
+				//pix = append(pix, pt)
+				//img.Draw(image.Rect(pt.X-5, pt.Y-5, pt.X+5, pt.Y+5), disp.Black, nil, image.ZP)
+			} else if m.Mb3() {
+				//pix = nil
+				img.Draw(disp.ScreenImage.R, disp.White, nil, image.ZP)
+			}
 			disp.Flush()
 		case <-ms.Resize:
 			// resized
@@ -45,11 +68,10 @@ loop:
 			}
 			img = disp.ScreenImage
 			img.Draw(disp.ScreenImage.R, disp.White, nil, image.ZP)
+			//for _, pt := range pix {
+			//	img.Draw(image.Rect(pt.X-5, pt.Y-5, pt.X+5, pt.Y+5), disp.Black, nil, image.ZP)
+			//}
 			disp.Flush()
-		case <-time.After(5 * time.Second):
-			// timeout, die
-			t.Logf("timeout")
-			break loop
 		}
 	}
 
